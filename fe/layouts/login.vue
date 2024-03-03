09a5f16cc9244601931fce08a145707dbfba8233
Łukasz <l.mordasiewicz@gmail.com>
2024-03-03 11:44:13 +0100

    feat: created login layout

---
+++ fe/layouts/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <main class="container mx-auto px-8 py-4">
+      <div class="max-w-[280px] mx-auto my-10 bg-gray-100 p-10 rounded-lg lg:max-w-[420px]">
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
+
+<style>
+body {
+  @apply bg-gray-700;
+}
+</style>
