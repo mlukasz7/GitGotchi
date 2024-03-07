d252f75fb5fc679816a9e433459d70eb11c522ff
Łukasz <l.mordasiewicz@gmail.com>
2024-03-07 16:47:05 +0100

    feat: added POC of happines component

---
+++ fe/components/MoodTv.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+  mood?: 'happy' | 'burnout';
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  mood: 'happy',
+});
+</script>
+
+<template>
+  <div class="relative mx-auto my-4 size-[460px] rounded-3xl border-[30px] border-black bg-white">
+    <img :src="`/moods/${props.mood}.webp`" />
+    <p class="absolute inset-x-0 -bottom-6 text-center text-sm text-white">
+      <b>Git</b>
+      Gotchi
+    </p>
+  </div>
+</template>
--- fe/pages/about.vue
+++ fe/pages/about.vue
@@ -5,8 +5,8 @@ definePageMeta({
 </script>
 
 <template>
-  <h1>About GG</h1>
-  <p class="my-4" v-for="i in new Array(100)" :key="i">
+  <MoodTv />
+  <p class="my-4" v-for="i in new Array(20)" :key="i">
     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, tenetur.
   </p>
 </template>
